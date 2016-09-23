457fd540aaded36257105843100e7ffa6479d57a
Zehaos <shizehao.scu@gmail.com>
2016-09-23 08:56:31 +0800

    Fix voc_label_parse.

--- lib/dataset_parser/voc_label_parse.py
+++ lib/dataset_parser/voc_label_parse.py
@@ -1,10 +1,10 @@
-import xml.dom.minidom
+#import xml.dom.minidom
+from xml.etree import ElementTree as ET
 
 classes_dict = {"aeroplane": 1, "bicycle": 2, "bird": 3, "boat": 4, "bottle": 5, "bus": 6,
                 "car": 7, "cat": 8, "chair": 9, "cow": 10, "diningtable": 11, "dog": 12, "horse": 13,
                 "motorbike": 14, "person": 15, "pottedplant": 16, "sheep": 17, "sofa": 18,
                 "train": 19, "tvmonitor": 20}
-
 
 class VocLabelParser:
     """
@@ -24,23 +24,20 @@ class VocLabelParser:
         """
         :return: [[class, difficult, xmin, xmax, ymin, ymax], ...]
         """
-        dom = xml.dom.minidom.parse(self.__file)
-        root = dom.documentElement
-        objects = root.getElementsByTagName("object")
-        label = []
-        for obj in objects:
-            # Get name
-            name = obj.getElementsByTagName('name')[0].childNodes[0].data
-            # Get difficult
-            difficult = obj.getElementsByTagName('difficult')[0].childNodes[0].data
-            # Get bbox
-            if len(obj.getElementsByTagName("bndbox")) == 0:
-                print self.__file + "contains no object."
-            for bboxs in obj.getElementsByTagName("bndbox"):
-                xmin = bboxs.getElementsByTagName('xmin')[0].childNodes[0].data
-                xmax = bboxs.getElementsByTagName('xmax')[0].childNodes[0].data
-                ymin = bboxs.getElementsByTagName('ymin')[0].childNodes[0].data
-                ymax = bboxs.getElementsByTagName('ymax')[0].childNodes[0].data
-                label.append([classes_dict[name], int(difficult), int(float(xmin)),
-                              int(float(xmax)), int(float(ymin)), int(float(ymax))])
-        return label
+        tree = ET.parse(self.__file)
+        root = tree.getroot()
+        labels = []
+        for object in root.findall('object'):
+            name = object.find('name').text
+            difficult = object.find('difficult').text
+            xmin =  object.find('bndbox').find('xmin').text
+            xmax = object.find('bndbox').find('xmax').text
+            ymin = object.find('bndbox').find('ymin').text
+            ymax = object.find('bndbox').find('ymax').text
+            labels.append([classes_dict[name], int(difficult), int(float(xmin)),
+                          int(float(xmax)), int(float(ymin)), int(float(ymax))])
+        return labels
+
+parser = VocLabelParser('/home/zehao/Dataset/VOC-DATASET/VOCdevkit/VOC2012/Annotations/2007_007763.xml')
+labels = parser.parse()
+print labels
